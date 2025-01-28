32e8a8f10e7c6cf00a0373e77e52144bc7c04caa
Darren Porciello <darren.porciello1@gmail.com>
2025-01-28 16:00:10 -0500

    Sidebar

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import React, { useState } from 'react';
+import Image from 'next/image';
+import { LockIcon } from 'lucide-react';
 
 type Props = {}
 
@@ -20,6 +22,20 @@ const Sidebar = () => {
                         EDLIST
                     </div>
                 </div>
+                {/* Team */}
+                <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
+                    <Image src="/logo.png" alt="Logo" width={40} height={40} />
+                    <div>
+                        <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
+                            Team Seven
+                        </h3>
+                        <div className="mt-1 flex items-start gap-2">
+                            <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
+                            <p className="text-xs text-gray-500">Private</p>
+                        </div>
+                    </div>                    
+                </div>
+                {/* NAVBAR LINKS */}
             </div>
         </div>
     )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { Inter } from "next/font/google";
+import "./globals.css";
 import DashboardWrapper from "./dashboardWrapper";
 
-const inter = Inter({subsets : ["latin"]});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,11 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={inter.className}
-      >
+      <body className={inter.className}>
         <DashboardWrapper>{children}</DashboardWrapper>
-        {children}
       </body>
     </html>
   );
