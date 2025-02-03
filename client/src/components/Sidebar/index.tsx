805ec0f436885bbd2cd8a4c688b281a7b9b1436d
Darren Porciello <darren.porciello1@gmail.com>
2025-02-03 11:27:32 -0500

    display project list

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -7,6 +7,7 @@ import { usePathname } from 'next/navigation';
 import { useAppDispatch, useAppSelector } from '@/app/redux';
 import Link from 'next/link';
 import { setIsSidebarCollapsed } from '@/state';
+import { useGetProjectsQuery } from '@/state/api';
 
 type Props = {}
 
@@ -14,6 +15,7 @@ const Sidebar = () => {
     const [showProjects, setShowProjects] = useState(true);
     const [showPriority, setShowPriority] = useState(true);
 
+    const { data:projects } = useGetProjectsQuery();
     const dispatch = useAppDispatch();
     const isSidebarCollapsed = useAppSelector(
         (state) => state.global.isSidebarCollapsed,
@@ -68,6 +70,17 @@ const Sidebar = () => {
                     <ChevronDown className="h-5 w-5" />
                 )}
                 </button>
+
+                {/* PROJECTS LIST */}
+                {showProjects &&
+                  projects?.map((project) => (
+                    <SidebarLink
+                      key={project.id}
+                      icon={Briefcase}
+                      label={project.name}
+                      href={`/projects/${project.id}`}
+                    />
+                  ))}
 
                 {/* Priorities links*/}
                 <button onClick={() => setShowPriority((prev) => !prev)}
