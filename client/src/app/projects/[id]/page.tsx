d6ef8c03839c628e01fc4fd8caaf32125bf0bfa5
Darren Porciello <darren.porciello1@gmail.com>
2025-02-07 20:22:09 -0500

    tabs for different views

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -4,6 +4,9 @@ import React, { useState } from "react";
 
 import ProjectHeader from "@/app/projects/ProjectHeader";
 import Board from "../BoardView";
+import List from "../ListView";
+import Timeline from "../TimelineView";
+import Table from "../TableView";
 
 type Props = {
   params: { id: string };
@@ -17,10 +20,19 @@ const Project = ({ params }: Props) => {
   return (
     <div>
         {/* MODAL NEW TASKS */}
-        <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
-        { activeTab === "Board" && (
-          <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
-        )}
+      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+      {activeTab === "Board" && (
+        <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "List" && (
+        <List id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "Timeline" && (
+        <Timeline id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "Table" && (
+        <Table id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
     </div>
   );
 };
