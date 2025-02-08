0765d7eaeda2964dae96e80bcccfbf49045b7fe7
Darren Porciello <darren.porciello1@gmail.com>
2025-02-08 15:31:29 -0500

    team interface addition

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -67,10 +67,18 @@ export interface SearchResults {
   users?: User[];
 }
 
+export interface Team {
+  teamId: number;
+  teamName: string;
+  productOwnerUserId?: number;
+  projectManagerUserId?: number;
+}
+
+
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL}),
     reducerPath: "api",
-    tagTypes: ["Projects", "Tasks", "Users"],
+    tagTypes: ["Projects", "Tasks", "Users", "Teams"],
     endpoints: (build) => ({
         getProjects: build.query<Project[], void>({
             query: () => "projects",
@@ -90,6 +98,13 @@ export const api = createApi({
               result
                 ? result.map(({ id }) => ({ type: "Tasks" as const, id }))
                 : [{ type: "Tasks" as const }],
+          }),
+          getTasksByUser: build.query<Task[], number>({
+            query: (userId) => `tasks/user/${userId}`,
+            providesTags: (result, error, userId) =>
+              result
+                ? result.map(({ id }) => ({ type: "Tasks", id }))
+                : [{ type: "Tasks", id: userId }],
           }),
         createTask: build.mutation<Task, Partial<Task>>({
             query: (task) => ({
@@ -115,7 +130,11 @@ export const api = createApi({
         }),
         search: build.query<SearchResults, string>({
           query: (query) => `search?query=${query}`,
-        })
+        }),
+        getTeams: build.query<Team[], void>({
+          query: () => "teams",
+          providesTags: ["Teams"],
+        }),
     }),
 });
 
@@ -127,4 +146,7 @@ export const {
   useCreateTaskMutation,
   useUpdateTaskStatusMutation,
   useSearchQuery,
-  useGetUsersQuery } = api;
+  useGetUsersQuery,
+  useGetTeamsQuery,
+  useGetTasksByUserQuery
+ } = api;
