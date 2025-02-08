93a3a48ca6abe8bdfba724a2e035c9f62dc692b2
Darren Porciello <darren.porciello1@gmail.com>
2025-02-07 23:24:15 -0500

    task and project modals

--- client/src/app/projects/ProjectHeader.tsx
+++ client/src/app/projects/ProjectHeader.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Header from "@/components/Header"
-import { Clock, Filter, Grid3x3, List, Share2, Table } from "lucide-react";
+import { Clock, Filter, Grid3x3, List, PlusSquare, Share2, Table } from "lucide-react";
+import ModalNewProject from "./ModalNewProject";
 
 type Props = {
     activeTab: string;
@@ -8,12 +9,26 @@ type Props = {
 };
 
 const ProjectHeader = ({activeTab, setActiveTab}: Props) => {
-    const [isModalNewProjectOpen, setIsModalNewprojectOpen] = useState(false);
+    const [isModalNewProjectOpen, setIsModalNewProjectOpen] = useState(false);
     return <div className="px-4 xl:px-6">
         {/* Modal new project */}
-        <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
-            <Header name = "Product Design Development "/>
-        </div>
+        <ModalNewProject
+            isOpen={isModalNewProjectOpen}
+            onClose={() => setIsModalNewProjectOpen(false)}
+        />
+      <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
+        <Header
+          name="Product Development"
+          buttonComponent={
+            <button
+              className="flex items-center rounded-md bg-blue-primary px-3 py-2 text-white hover:bg-blue-600"
+              onClick={() => setIsModalNewProjectOpen(true)}
+            >
+              <PlusSquare className="mr-2 h-5 w-5" /> New Boards
+            </button>
+          }
+        />
+      </div>
 
         {/* Tabs */}
         <div className="flex flex-wrap-reverse gap-2 border-y border-gray-200 pb-[8px] pt-2 dark:border-stroke-dark md:items-center">
--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -8,6 +8,7 @@ import Board from "../BoardView";
 import List from "../ListView";
 import Timeline from "../TimelineView";
 import Table from "../TableView";
+import ModalNewTask from "@/components/ModalNewTask"
 
 const Project = () => {
   const params = useParams();  // Use useParams() to get the params object
@@ -19,6 +20,11 @@ const Project = () => {
   return (
     <div>
       {/* MODAL NEW TASKS */}
+      <ModalNewTask
+        isOpen={isModalNewTaskOpen}
+        onClose={() => setIsModalNewTaskOpen(false)}
+        id={id}
+      />
       <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
       {activeTab === "Board" && (
         <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
