cd66885e6b8c6302d2a70d72443716d9119ac0ed
Darren Porciello <darren.porciello1@gmail.com>
2025-02-07 22:02:15 -0500

    next.js 15 param changes

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
+import { useParams } from "next/navigation";  // Import useParams
 
 import ProjectHeader from "@/app/projects/ProjectHeader";
 import Board from "../BoardView";
@@ -8,18 +9,16 @@ import List from "../ListView";
 import Timeline from "../TimelineView";
 import Table from "../TableView";
 
-type Props = {
-  params: { id: string };
-};
+const Project = () => {
+  const params = useParams();  // Use useParams() to get the params object
+  const { id } = params as { id: string };  // Ensure correct type
 
-const Project = ({ params }: Props) => {
-  const { id } = params;
   const [activeTab, setActiveTab] = useState("Board");
   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
 
   return (
     <div>
-        {/* MODAL NEW TASKS */}
+      {/* MODAL NEW TASKS */}
       <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
       {activeTab === "Board" && (
         <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
