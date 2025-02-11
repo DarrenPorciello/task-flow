516d082d6d70149e1aaa5465ed0ce0e881f5b948
Darren Porciello <darren.porciello1@gmail.com>
2025-02-11 10:12:09 -0500

    fixed redux bug

--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -80,7 +80,7 @@ export default function StoreProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore>();
+  const storeRef = useRef<AppStore | null>(null); // ✅ Provide an initial value
   if (!storeRef.current) {
     storeRef.current = makeStore();
     setupListeners(storeRef.current.dispatch);
