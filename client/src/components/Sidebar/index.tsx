e2abf3e945fe36cd7d5913b0389b757934cf6671
Darren Porciello <darren.porciello1@gmail.com>
2025-02-12 12:14:56 -0500

    authentication and authorization with cognito

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -7,7 +7,8 @@ import { usePathname } from 'next/navigation';
 import { useAppDispatch, useAppSelector } from '@/app/redux';
 import Link from 'next/link';
 import { setIsSidebarCollapsed } from '@/state';
-import { useGetProjectsQuery } from '@/state/api';
+import { useGetAuthUserQuery, useGetProjectsQuery } from '@/state/api';
+import { signOut } from 'aws-amplify/auth';
 
 type Props = {}
 
@@ -19,7 +20,19 @@ const Sidebar = () => {
     const dispatch = useAppDispatch();
     const isSidebarCollapsed = useAppSelector(
         (state) => state.global.isSidebarCollapsed,
-      );
+    );
+
+    const { data: currentUser } = useGetAuthUserQuery({});
+    const handleSignOut = async () => {
+      try {
+        await signOut();
+      } catch (error) {
+        console.error("Error signing out: ", error);
+      }
+    };
+    if (!currentUser) return null;
+    const currentUserDetails = currentUser?.userDetails;
+
 
     const sidebarClassNames = `fixed flex flex-col h-[100%] justfy-between shadow-xl transition-all duration-300 h-full z-40
      dark:bg-black overflow-y-auto bg-white w-64 md:w-64 ${isSidebarCollapsed ? "w-0 hidden" : "w-64"}`;
@@ -114,10 +127,33 @@ const Sidebar = () => {
             />
           </>
         )}
-
-
-                {/* Projects List */}
+          </div>
+          <div className="z-10 mt-32 flex w-full flex-col items-center gap-4 bg-white px-8 py-4 dark:bg-black md:hidden">
+            <div className="hidden items-center justify-between md:flex">
+                <div className="flex w-full items-center">
+                    {!!currentUserDetails?.profilePictureUrl ? (
+                    <Image
+                        src={`https://pm-s3-images-all.s3.us-east-1.amazonaws.com//${currentUserDetails?.profilePictureUrl}`}
+                        alt={currentUserDetails?.username || "User Profile Picture"}
+                        width={100}
+                        height={50}
+                        className="h-full rounded-full object-cover"
+                    />
+                    ) : (
+                    <User className="h-6 w-6 cursor-pointer self-center rounded-full dark:text-white" />
+                    )}
+                </div>
+            <span className="mx-3 text-gray-800 dark:text-white">
+                {currentUserDetails?.username}
+            </span>
+            <button
+                className="self-start rounded bg-blue-400 px-4 py-2 text-xs font-bold text-white hover:bg-blue-500 md:block"
+                onClick={handleSignOut}
+            >
+                Sign out
+            </button>
             </div>
+          </div>
         </div>
     )
 };
